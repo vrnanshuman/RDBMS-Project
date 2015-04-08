96e37f31e45d2f296897cabfcc1ef65a831d24cf
Ashish Mehrotra <ashishm1995@gmail.com>
2015-04-08 14:43:46 +0530

    Purchase and Sales completed

---
+++ Inventory Manager/src/inventory/manager/InventoryManager.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventory.manager;
+
+/**
+ *
+ * @author Mahe
+ */
+public class InventoryManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ApplicationPage ap = new ApplicationPage();
+        ap.setVisible(true);
+    }
+}
